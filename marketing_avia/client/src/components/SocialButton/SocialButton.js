39550dea4b1580810326e2ac3f23e8ee2211c964
Evgeniy <nifpopnaf@gmail.com>
2020-12-10 13:34:30 +0400

    styles buttons share done

--- marketing_avia/client/src/components/SocialButton/SocialButton.js
+++ marketing_avia/client/src/components/SocialButton/SocialButton.js
@@ -1,13 +1,62 @@
+import React, { useState } from "react"
+
 import "./SocialButton.css"
 
 import heart from "../../assets/icons/heart.svg"
-import vk from "../../assets/icons/vk.svg"
+import _vk from "../../assets/icons/vk.svg"
+import _facebook from "../../assets/icons/facebook.svg"
+import _twitter from "../../assets/icons/twitter.svg"
+import _ok from "../../assets/icons/ok.svg"
 
-const SocialButton = () => {
+const typeButtons = {
+	facebook: _facebook,
+	ok: _ok,
+	twitter: _twitter,
+	vk: _vk,
+}
+
+
+const colorButtons = {
+	facebook: "btn--facebook",
+	ok: "btn--ok",
+	twitter: "btn--twitter",
+	vk: "btn--vk",
+}
+
+const linkButtons = {
+	facebook:
+		"https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Faviasales.ru%2F&amp;src=sdkpreparse",
+	ok: "https://connect.ok.ru/offer?url=https%3A%2F%2aviasales.ru",
+	twitter:
+		"https://twitter.com/intent/tweet?text=Смотри%20что%20я%20нашел%20на%20Aviasales.ru&url=https%3A%2F%2Faviasales.ru%2F",
+	vk: "https://vk.com/share.php?url=https%3A%2F%2Faviasales.ru%2FShare",
+}
+
+
+const shareButton = (link) => {
+	const linkBtn = linkButtons[link]
+
+	let shareWindow = window.open(
+		linkBtn,
+		"Птички лети, лети",
+		"width=700,height=500"
+	)
+}
+
+const SocialButton = (props) => {
+	const { socail } = props
+	const srcBtn = typeButtons[socail]
+	const colorBtn = colorButtons[socail]
+	let styles = ["button-component button-component--share"]
+	styles = styles.concat(colorBtn)
+
+
   return (
-		<div className="button-component">
-			<img src={vk}></img>
-		</div>
+		<button
+			className={styles.join(" ")}
+			onClick={() => shareButton(socail)}
+		>
+		</button>
 	)
 }
 
--- marketing_avia/client/src/containers/Social.js
+++ marketing_avia/client/src/containers/Social.js
@@ -2,15 +2,17 @@ import "./Social.css"
 import SocialButton from '../components/SocialButton/SocialButton'
 
 const Social = () => {
+	
+
   return (
 		<div className="container__social">
 			<div className="wrapper">
 				<p className="container__social-label">Поделись с друзьями:</p>
 				<div className="container__buttons">
-					<SocialButton></SocialButton>
-					<SocialButton></SocialButton>
-					<SocialButton></SocialButton>
-					<SocialButton></SocialButton>
+					<SocialButton socail="vk"></SocialButton>
+					<SocialButton socail="facebook"></SocialButton>
+					<SocialButton socail="twitter"></SocialButton>
+					<SocialButton socail="ok"></SocialButton>
 				</div>
 			</div>
 		</div>
